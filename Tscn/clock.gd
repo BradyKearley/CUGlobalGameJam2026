170f54fa9d8824733d7c542d4acc3a526fcc44e2
Brady Raye Kearley <bradykearley84@gmail.com>
2026-01-31 16:38:51 -0500

    Clock hands

--- Tscn/clock.gd
+++ Tscn/clock.gd
@@ -2,4 +2,4 @@ extends MultiMeshInstance3D
 @export var clockRingTime: int
 
 func _ready() -> void:
-	$Marker3D/Timer.start(clockRingTime)
+	$HourHand/Timer.start(clockRingTime)
--- Tscn/marker_3d.gd
+++ Tscn/marker_3d.gd
@@ -1,9 +1,12 @@
 extends Marker3D
-@export var loopRatio: int
+@export var loopRatio: float
+@export var starting_rotation: float = 0.0
+
 func _ready():
+	rotation.z = deg_to_rad(starting_rotation)
 	var tween = create_tween()
 	tween.set_loops()
-	tween.tween_method(_rotate_z, 0.0, 360.0, loopRatio * 120.0)
+	tween.tween_method(_rotate_z, starting_rotation, starting_rotation + 360.0, loopRatio * 120.0)
 
 func _rotate_z(angle_degrees: float):
 	rotation.z = - deg_to_rad(angle_degrees)
