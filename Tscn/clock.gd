262fa00ac76aa593269987e129df67bd7fcc42d2
Brady Raye Kearley <bradykearley84@gmail.com>
2026-01-31 15:03:12 -0500

    Added some lighting and fixed other stuff

--- Tscn/clock.gd
+++ Tscn/clock.gd
@@ -1,5 +1,5 @@
 extends MultiMeshInstance3D
-@export var clockRingTime:int
+@export var clockRingTime: int
 
 func _ready() -> void:
-	$Marker3D/Timer.start(clockRingTime) 
+	$Marker3D/Timer.start(clockRingTime)
